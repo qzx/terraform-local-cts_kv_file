e81aafb8670131a6f98ea83c96f3270319b53820
Freyr Guðjónsson <ze@anime.is>
2022-03-04 15:11:32 +0000

    KV only

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
-resource "local_file" "address" {
-  for_each = var.services
-  content  = each.value.address
-  filename = "resources/${each.value.id}.txt"
-}
-
 resource "local_file" "key-value-pairs" {
   for_each = var.consul_kv
   content  = each.value
